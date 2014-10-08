ab6d511668a75ff4586144f662532065e09bf843
Jade Meskill <jade.meskill@gmail.com>
2014-10-08 12:07:32 -0700

    add greeting wrapper

---
+++ command.js
@@ -0,0 +1,16 @@
+var Plugin = require('./index').Plugin;
+var skynet = require('skynet');
+var config = require('./meshblu.json');
+
+var conx = skynet.createConnection({
+  server: 'meshblu.octoblu.com',
+  port: '80',
+  uuid: config.uuid,
+  token: config.token
+});
+
+conx.on('notReady', console.error);
+conx.on('error', console.error);
+
+var plugin = new Plugin();
+conx.on('message', plugin.onMessage);
