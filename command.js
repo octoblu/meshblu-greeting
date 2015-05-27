87d41bcf7bccf9382bd5a989c152d7ff475e5975
Roy van de Water <roy.vwater@gmail.com>
2015-05-27 16:28:20 -0700

    Auto deploy to s3, v1.0.4

--- command.js
+++ command.js
@@ -1,23 +1,5 @@
-var Plugin = require('./index').Plugin;
-var meshblu = require('meshblu');
+'use strict';
 var config = require('./meshblu.json');
+var Connector = require('./connector');
 
-var conx = meshblu.createConnection({
-  server: 'meshblu.octoblu.com',
-  port: '80',
-  uuid: config.uuid,
-  token: config.token
-});
-
-conx.on('notReady', console.error);
-conx.on('error', console.error);
-
-var plugin = new Plugin(conx, {});
-conx.on('message', function(){
-  try {
-    plugin.onMessage.apply(plugin, arguments);
-  } catch (error){
-    console.error(error.message);
-    console.error(error.stack);
-  }
-});
+new Connector(config);
